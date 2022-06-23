7fd4d81fb7e54cf952b1a8b485c23d96b842c028
jacoposergio <djago32@gmail.com>
2022-06-23 15:55:16 +0200

    stampato

--- script/script.js
+++ script/script.js
@@ -2,3 +2,12 @@
 // TODO| Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // TODO| BONUS:
 // TODO| Aggiungere alla pagina un’immagine, presa anch’essa da un data (in data avrò l'src).
+
+var app= new Vue(
+    {
+        el: '#root',
+        data: {
+            titleClass: 'Ciao, sono un H1'
+        }
+    }
+)
